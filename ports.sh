6e8cb6c64f57a2ab5d532337631e5d5dcb7c6f09
CBAZs <73243917+CBAZs@users.noreply.github.com>
2022-11-16 21:56:35 -0600

    Add files via upload

---
+++ ports.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+host=$1
+firstport=$2
+lastport=$3
+
+function portscanner {
+         for((counter==$firstport; counter<=$lastport; counter++))
+do
+         (echo >/dev/tcp/$host/$counter) > /dev/null 2>&1 && echo "$counter open"
+done
+}
+portscanner
